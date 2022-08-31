274ce99c5287701a41ab4c39021cb30c87ffcd47
raja <raja.rakoto7@gmail.com>
2022-08-31 19:37:56 +0300

    utils: valid concatResult exportation -> ampalibe.code-snippets

--- utils/engine.js
+++ utils/engine.js
@@ -1,4 +1,5 @@
 import traverse from 'traverse';
+import fs from 'fs';
 import { basic } from '../src/basic.js';
 import { chat } from '../src/chat.js';
 import { deps } from '../src/deps.js';
@@ -23,4 +24,33 @@ import { ui } from '../src/ui.js';
 // 	return [parentKeyList, childKeyList];
 // }
 
-console.log(chat);
+// snippets data
+let sourceList = [basic, chat, deps, query, ui];
+let concatResult = '{';
+
+// concat snippets data
+sourceList.forEach(source => {
+	source = JSON.stringify(source);
+	source = source.slice(1, source.length - 1);
+	concatResult += source + ',';
+});
+
+concatResult = concatResult.slice(0, concatResult.length - 2) + '}}';
+
+// to ampalibe code snippets
+fs.writeFileSync(
+	'../dist/ampalibe.code-snippets',
+	concatResult,
+	'utf8',
+	err => {
+		console.log(err ? err : 'The file was saved!');
+	},
+);
+
+const distFile = JSON.parse(fs.readFileSync('../dist/ampalibe.code-snippets', 'utf8'));
+
+traverse(distFile).forEach(function (e) {
+	if (this.key == "prefix") {
+		console.log(e)
+	}
+});
