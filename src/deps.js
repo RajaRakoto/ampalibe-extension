5ea999e40996a6df0456dc17b3f995cf8d7dd91d
raja <raja.rakoto7@gmail.com>
2022-09-01 13:25:47 +0300

    src: add amp nat & config snippets

--- src/deps.js
+++ src/deps.js
@@ -1,4 +1,38 @@
 export const deps = {
+	"[deps]: version>=1.1.4 - Import the native module for ampalibe": {
+		"prefix": "amp-import-nat-latest",
+		"body": [
+			"import ampalibe",
+			"from ampalibe import Model, Messenger",
+			"",
+			"chat = Messenger()",
+			"query = Model()",
+			"",
+			"chat.get_started()"
+		],
+		"description": "[deps]: version>=1.1.4 - Import the native module for ampalibe"
+	},
+	"[deps]: version<=1.0.7 - Import the native module for ampalibe": {
+		"prefix": "amp-import-nat",
+		"body": [
+			"import ampalibe",
+			"from conf import Configuration",
+			"",
+			"bot = ampalibe.init(Configuration())",
+			"chat = bot.chat",
+			"query = bot.query",
+			"",
+			"chat.get_started()"
+		],
+		"description": "[deps]: version<=1.0.7 - Import the native module for ampalibe"
+	},
+	"[deps]: Import config": {
+		"prefix": "amp-import-config",
+		"body": [
+			"from conf import Configuration as config"
+		],
+		"description": "[deps]: Import config"
+	},
   "[deps]: import quick_reply deps": {
 		"prefix": "amp-import-quickreply",
 		"body": ["from ampalibe.ui import QuickReply"],
