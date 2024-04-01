abc4b5a3830e630d60fff377b20f91188fa5d251
raja <raja.rakoto7@gmail.com>
2024-04-01 16:15:44 +0300

    add extension scripts

---
+++ build.js
@@ -0,0 +1,39 @@
+/* libs */
+import fs from "fs";
+import path from "path";
+
+/* src snippets sources */
+import { pattern } from './src/pattern.js';
+import { basic } from './src/basic.js';
+import { chat } from './src/chat.js';
+import { deps } from './src/deps.js';
+import { query } from './src/query.js';
+import { ui } from './src/ui.js';
+
+// ========================================
+
+// to ensure that the directory exists, creating it if necessary
+function ensureDirectoryExists(directory) {
+	if (!fs.existsSync(directory)) {
+		fs.mkdirSync(directory, { recursive: true });
+	}
+}
+
+// all snippets list
+const snippetsList = [pattern, basic, chat, deps, query, ui];
+
+// merge all snippets
+const concatenedSnippets = Object.assign(
+	{},
+	...snippetsList.map((snippet) => {
+		return snippet;
+	}),
+);
+
+// convert to JSON
+const json = JSON.stringify(concatenedSnippets);
+
+// write to file
+const distDirectory = "./dist";
+ensureDirectoryExists(distDirectory);
+fs.writeFileSync(path.join(distDirectory, "ampalibe-extension.code-snippets"), json);
